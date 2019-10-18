549de91d963b431836f77efd899212a3359c4d5a
rehanvipin <43005490+rehanvipin@users.noreply.github.com>
2019-10-18 19:01:57 +0530

    Fixed both

--- py/fibonacci.py
+++ py/fibonacci.py
@@ -1,7 +1,22 @@
-#function to fibonacci sequence
-
 def fibonacci(n):
-   if n < 1:
-       return n
-   else:
-        return(fibonacci(n-1) + fibonacci(n-2))
+	a = 0
+	b = 1
+	for i in range(n):
+		b = b+a
+		a = b-a
+	return a
+
+def fibgen(n):
+	a = 0
+	b = 1
+	for i in range(n):
+		b = b+a
+		a = b-a
+		yield a
+		
+
+# print(fibonacci(6))
+
+# for i in fibgen(10):
+# 	print(i)
+
