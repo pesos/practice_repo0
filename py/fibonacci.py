a3f793768171233616e92b2c6e99b483d1862ad0
Shishir <shishir.srik@gmail.com>
2019-10-19 06:37:55 +0530

    corrected the fibonacci function

--- py/fibonacci.py
+++ py/fibonacci.py
@@ -1,7 +1,11 @@
 #function to fibonacci sequence
 
 def fibonacci(n):
-   if n < 1:
-       return n
-   else:
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
         return(fibonacci(n-1) + fibonacci(n-2))
+
+print(fibonacci(13))
